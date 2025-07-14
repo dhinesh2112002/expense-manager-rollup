1721b00acd5e9c23bb07f811186b4a926a9b3c46
Dhinesh <dhinesh.r@newgensoft.com>
2025-07-14 18:19:35 +0530

    Update HelloWorld to class component

--- public/bundle.js
+++ public/bundle.js
@@ -548,7 +548,7 @@
 
 	class HelloWorld extends React.Component {
 	  render() {
-	    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+	    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Natha da leo! leo das"));
 	  }
 	}
 
